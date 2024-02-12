98d2da827f75e132f8c624312ff64eaf901434ff
Jack Maders <jackwmaders@gmail.com>
2024-02-12 23:07:19 +0000

    add structure for wordpress theme

---
+++ theme/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+function tutorial_theme_support()
+{
+    add_theme_support("title-tag");
+    add_theme_support("custom-logo");
+    add_theme_support("post-thumbnails");
+}
+
+function tutorial_register_styles()
+{
+    $version = wp_get_theme()->get("Version");
+
+    wp_enqueue_style(
+        "main",
+        get_template_directory_uri() . "/assets/css/style.css",
+        [],
+        $version
+    );
+}
+
+function tutorial_register_scripts()
+{
+    $version = wp_get_theme()->get("Version");
+
+    wp_enqueue_script(
+        "main",
+        get_template_directory_uri() . "/assets/js/index.js",
+        [],
+        $version
+    );
+}
+
+add_action("init", "tutorial_theme_support");
+add_action("wp_enqueue_scripts", "tutorial_register_styles");
+add_action("wp_enqueue_scripts", "tutorial_register_scripts");
---
+++ theme/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <main class="container">
+        <?php get_template_part('templates/navigation') ?>
+
+
+        <?php get_template_part('templates/footer') ?>
+    </main>
+
+
+    <?php wp_footer(); ?>
+</body>
+
+
+</html>
