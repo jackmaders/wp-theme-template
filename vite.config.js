b14ea10ef324dde387f0479f01a476c1671634db
Jack Maders <jackwmaders@gmail.com>
2024-02-12 22:35:05 +0000

    add vite plugin

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,47 @@ import FastGlob from "fast-glob";
 import { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "main",
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    publicDir: "theme",
+    build: {
+      outDir: process.env.VITE_OUTPUT_DIR ?? "dist",
+      emptyOutDir: true,
+      lib: {
+        entry: resolve(__dirname, "src/main.ts"),
+        name: "main",
+      },
+      rollupOptions: {
+        output: {
+          chunkFileNames: "assets/js/[name].js",
+          entryFileNames: "assets/js/[name].js",
+
+          assetFileNames: ({ name }) => {
+            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+              return "assets/images/[name][extname]";
+            }
+
+            if (/\.css$/.test(name ?? "")) {
+              return "assets/css/[name][extname]";
+            }
+
+            return "assets/[name][extname]";
+          },
+        },
+      },
     },
-  },
-});
+    plugins: [
+      {
+        name: "watch-external", // https://stackoverflow.com/questions/63373804/rollup-watch-include-directory/63548394#63548394
+        async buildStart() {
+          const files = await FastGlob(["theme/**/*"]);
+          for (let file of files) {
+            this.addWatchFile(file);
+          }
+        },
+      },
+    ],
+  });
+};
