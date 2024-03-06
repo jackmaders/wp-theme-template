a16740856e5816433651e71d0a6f9df44397c843
Jack Maders <jackwmaders@gmail.com>
2024-03-06 21:13:24 +0000

    rename function prefix

--- theme/functions/head.php
+++ theme/functions/head.php
@@ -1,6 +1,6 @@
 <?php
 
-function threeGen_register_styles()
+function template_register_styles()
 {
     $version = wp_get_theme()->get("Version");
 
@@ -12,7 +12,7 @@ function threeGen_register_styles()
     );
 }
 
-function threeGen_register_scripts()
+function template_register_scripts()
 {
     $version = wp_get_theme()->get("Version");
 
@@ -24,12 +24,12 @@ function threeGen_register_scripts()
     );
 }
 
-function threeGen_meta_viewport()
+function template_meta_viewport()
 {
     echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
 }
 
-add_action("wp_enqueue_scripts", "threeGen_register_styles");
-add_action("wp_enqueue_scripts", "threeGen_register_scripts");
+add_action("wp_enqueue_scripts", "template_register_styles");
+add_action("wp_enqueue_scripts", "template_register_scripts");
 
-add_action("wp_head", "threeGen_meta_viewport", 1);
+add_action("wp_head", "template_meta_viewport", 1);
--- theme/functions/theme_support.php
+++ theme/functions/theme_support.php
@@ -1,9 +1,9 @@
 <?php
-function wpTemplate_theme_support()
+function template_theme_support()
 {
     add_theme_support("title-tag");
     add_theme_support("custom-logo");
     add_theme_support("post-thumbnails");
 }
 
-add_action("init", "wpTemplate_theme_support");
+add_action("init", "template_theme_support");
